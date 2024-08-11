0922a67a73c62ca88b3f8be3c18886f4cb42805f
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-08-11 15:41:08 +0100

    development

--- src/components/posts/editors/mutations.ts
+++ src/components/posts/editors/mutations.ts
@@ -7,16 +7,25 @@ import {
 } from "@tanstack/react-query";
 import { submitPost } from "./actions";
 import { PostPage } from "@/lib/types";
+import { useSession } from "@/app/(main)/session-provider";
 
 export function useSubmitPostMutation() {
   const { toast } = useToast();
 
   const queryClient = useQueryClient();
 
+  const { user } = useSession();
+
   const mutation = useMutation({
     mutationFn: submitPost,
     onSuccess: async (newPost) => {
-      const queryFilter: QueryFilters = { queryKey: ["post-feed", "for-you"] };
+      const queryFilter = {
+        queryKey: ["post-feed"],
+        predicate: (query) =>
+          query.queryKey.includes("for-you") ||
+          (query.queryKey.includes("user-posts") &&
+            query.queryKey.includes(user.id)),
+      } satisfies QueryFilters;
 
       await queryClient.cancelQueries(queryFilter);
 
@@ -43,7 +52,7 @@ export function useSubmitPostMutation() {
       queryClient.invalidateQueries({
         queryKey: queryFilter.queryKey,
         predicate(query) {
-          return !query.state.data;
+          return queryFilter.predicate(query) && !query.state.data;
         },
       });
 
